1874633b687c2ed36edb134a398599c9eb71c45e
Aniket Pal <67703407+Aniket762@users.noreply.github.com>
2020-09-03 15:51:33 +0530

    Initial
    AR Foundation and XR packages must be installed

---
+++ Assets/cube.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    public GameObject objectToSpawn;
+    private PlacementIndicator placementIndicator;
+
+    void start()
+    {
+        placementIndicator = FindObjectofType<PlacementIndicator>();
+
+    }
+    void Update()
+    {
+        if(Input.touchCount > 0  && Input.touches[0].phase == TouchPhase.Began)
+        {
+            GameObject obj = Instantitate(objectToSpawn,placementIndicator.transform.position,placementIndicator.transform.rotation);
+            
+        }
+    }
+
+}
